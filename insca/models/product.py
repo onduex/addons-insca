2efc01b8212df12b4f64a5d4c03d9baaab454369
onduex <tomas.pascual@onduex.com>
2021-10-19 12:43:42 +0200

    [IMP] fields updated

--- insca/models/product.py
+++ insca/models/product.py
@@ -7,11 +7,14 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    vault_length = fields.Char(string='Vault Longitud', required=False)
-    vault_height = fields.Char(string='Vault Alto', required=False)
+    vault_length = fields.Char(string='Vault Largo', required=False)
+    vault_width = fields.Char(string='Vault Ancho', required=False)
+    vault_height = fields.Char(string='Vault Hondo', required=False)
     vault_thinkness = fields.Char(string='Vault Espesor', required=False)
     vault_route = fields.Char(string='Vault Ruta', required=False)
     vault_code = fields.Char(string='Vault Código', required=False)
     vault_material = fields.Char(string='Vault Material', required=False)
     vault_color = fields.Char(string='Vault Color', required=False)
     vault_categ = fields.Char(string='Vault Categoría', required=False)
+
+
