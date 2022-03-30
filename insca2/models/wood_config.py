0ed8d0f8a07b4255367756ef0adb49e54b48eaa7
onduex <tomas.pascual@onduex.com>
2022-03-30 16:49:03 +0200

    [IMP] update insca2 add wood config

--- insca2/models/wood_config.py
+++ insca2/models/wood_config.py
@@ -13,30 +13,33 @@ class WoodConfig(models.Model):
 
     name = fields.Char(string='Código', required=False, store=True, readonly=True)
     color_madera_id = fields.Many2one(comodel_name='product.template',
-                                      string='Color madera',
+                                      string='Color pieza',
                                       required=False,
                                       domain="[('categ_base', '=', 'COLOR MADERA')]")
     cantos_id = fields.Many2one(comodel_name='product.template',
-                                string='Canto',
+                                string='Chapa cantos',
                                 required=False,
                                 domain="[('categ_base', '=', 'CANTO')]")
 
     @api.model
     def create(self, vals):
         res = super(WoodConfig, self).create(vals)
+        used_keywords = []
         wood_config_ids = self.env['wood.config'].search([])
-
         keywords = sorted([''.join(i) for i in product(ascii_uppercase, repeat=3, )], reverse=True)
-        # print(keywords[res.id])
-        res.update({'name': keywords[res.id],
-                    })
+
         new_string2compare = str(res.color_madera_id.id) + str(res.cantos_id.id)
         print('New sting: ', new_string2compare)
 
         for record in wood_config_ids[:-1]:
+            used_keywords.append(record.name)
             existing_string2compare = str(record.color_madera_id.id) + str(record.cantos_id.id)
             print('Existing sting: ', existing_string2compare)
             if new_string2compare == existing_string2compare:
                 raise ValidationError(_('La nueva combinación existe con el código %s' % record.name))
+
+        difference = list(set(keywords).difference(used_keywords))
+        difference.sort(reverse=True)
+        res.update({'name': difference[0], })
 
         return res
