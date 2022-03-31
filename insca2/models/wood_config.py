dc5e66f86551f69af1b97593aa13bc847d65b032
onduex <tomas.pascual@onduex.com>
2022-03-31 10:11:41 +0200

    [IMP] update insca2

--- insca2/models/wood_config.py
+++ insca2/models/wood_config.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
 from itertools import product
 from string import ascii_uppercase
+
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
@@ -20,10 +21,11 @@ class WoodConfig(models.Model):
                                 string='Chapa cantos',
                                 required=False,
                                 domain="[('categ_base', '=', 'CANTO')]")
-    producto_categoria_tipo = fields.Many2one(comodel_name='product.category.type',
-                                              string='Color cantos',
-                                              required=False,
-                                              domain="[]")
+    color_cantos_id = fields.Many2one(comodel_name='product.template',
+                                      string='Color cantos',
+                                      required=False,
+                                      domain="[('categ_base', '=', 'COLOR MADERA')]")
+    obsoleto = fields.Boolean(string='Obsoleto', required=False)
 
     @api.model
     def create(self, vals):
@@ -32,12 +34,14 @@ class WoodConfig(models.Model):
         wood_config_ids = self.env['wood.config'].search([])
         keywords = sorted([''.join(i) for i in product(ascii_uppercase, repeat=3, )], reverse=True)
 
-        new_string2compare = str(res.color_madera_id.id) + str(res.cantos_id.id)
+        new_string2compare = str(res.color_madera_id.id) + str(res.cantos_id.id) + str(res.color_cantos_id.id)
         print('New sting: ', new_string2compare)
 
         for record in wood_config_ids[:-1]:
             used_keywords.append(record.name)
-            existing_string2compare = str(record.color_madera_id.id) + str(record.cantos_id.id)
+            existing_string2compare = str(record.color_madera_id.id) + \
+                                      str(record.cantos_id.id) + \
+                                      str(record.color_cantos_id.id)
             print('Existing sting: ', existing_string2compare)
             if new_string2compare == existing_string2compare:
                 raise ValidationError(_('La nueva combinación existe con el código %s' % record.name))
