627c2f09e22c09f23f88a55ed288c64271bd0983
onduex <tomas.pascual@onduex.com>
2022-04-25 14:45:31 +0200

    [IMP] update insca

--- insca/models/mrp_bom.py
+++ insca/models/mrp_bom.py
@@ -3,6 +3,7 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+from collections import OrderedDict
 
 
 class MrpBom(models.Model):
@@ -16,6 +17,8 @@ class MrpBom(models.Model):
         product_id = vals_list[0]['product_tmpl_id']
         product_for_bom = self.env['product.template'].search([('id', '=', product_id)])
         mrp_routing = self.env['mrp.routing'].search([('name', '=', product_for_bom.vault_route)])
+        product_ids = []
+        lines = []
 
         for bom in res:
             res_code = self.env['res.code'].search([('name', '=', bom.product_tmpl_id.vault_code)])
@@ -27,22 +30,20 @@ class MrpBom(models.Model):
                                         % (bom.product_tmpl_id.vault_route, bom.product_tmpl_id.name)))
             elif product_for_bom.vault_route and len(mrp_routing):
                 res.update({'routing_id': mrp_routing.id})
+
+            if bom.product_tmpl_id.vault_code == 'A31P' and \
+                    bom.product_tmpl_id.default_code[-3:] != '000' and \
+                    bom.is_vault_bom:
+                print('############################')
+                product_ids += self.env['product.product']. \
+                    search([('inventor_color', '=', bom.product_tmpl_id.vault_color)])
+                product_ids += self.env['product.product']. \
+                    search([('default_code', '=', bom.product_tmpl_id.default_code[:-3] + '000')])
+                for product in product_ids:
+                    lines.append((0, 0, {'product_id': product.id, 'product_qty': 1}))
+                res.update({'bom_line_ids': lines})
+
         return res
-
-    def write(self, vals_list):
-        lines = []
-        product_ids = []
-
-        if self.product_tmpl_id.vault_code == 'A31P' and self.is_vault_bom:
-
-            product_ids += self.env['product.product'].\
-                search([('inventor_color', '=', self.product_tmpl_id.vault_color)])
-            product_ids += self.env['product.product'].\
-                search([('default_code', '=', self.product_tmpl_id.default_code[:-3] + '000')])
-            for product in product_ids:
-                lines.append((0, 0, {'product_id': product.id, 'product_qty': 1}))
-            vals_list.update({'bom_line_ids': lines})
-        return super(MrpBom, self).write(vals_list)
 
 
 class MrpBomLine(models.Model):
@@ -54,6 +55,7 @@ class MrpBomLine(models.Model):
         for record in res:
             # Código A31 borrar líneas de BoM
             if record.bom_id.product_tmpl_id.vault_code == 'A31P' and \
+                    record.bom_id.product_tmpl_id.default_code[-3:] != '000' and \
                     record.bom_id.is_vault_bom and \
                     record.product_tmpl_id.vault_code == 'A30':
                 print('Delete %s of %s' % (record.display_name, record.bom_id.product_tmpl_id.default_code))
