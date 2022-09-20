f0fc51826c7a0a8a455c767295ec96715dae0d57
onduex <tomas.pascual@onduex.com>
2022-09-20 13:29:55 +0200

    [IMP] insca4 update

--- insca4/models/supplier_list.py
+++ insca4/models/supplier_list.py
@@ -3,6 +3,8 @@
 from odoo import fields, models, api, _
 from datetime import date
 
+po_line_id_metal_list = []
+
 
 def dynamic_selection():
     today = str(date.today().strftime("%d/%m/%Y"))
@@ -15,25 +17,29 @@ class Supplierlist(models.Model):
     _description = "Lista para proveedores de las piezas/ensamblajes requeridos de los pedidos activos"
     _order = "type_model_id, id asc"
 
+    sale_order = fields.Many2one(comodel_name='sale.order', string='Venta', required=False, readonly=True)
+    purchase_order = fields.Many2one(comodel_name='purchase.order', string='Compra ', required=False, readonly=True)
+    mrp_production = fields.Many2one(comodel_name='mrp.production', string='Fabricación', required=False, readonly=True)
+    product_template = fields.Many2one(comodel_name='product.template', string='Código + Nombre', required=False,
+                                       readonly=True)
+    vault_web_link = fields.Char(string='Vault Link', required=False, readonly=True)
     commitment_date = fields.Datetime(string='Fecha prevista', required=False)
     partner_name = fields.Char(string='Cliente', required=False, readonly=True)
-    manufacturing_origin = fields.Char(string='Fabricación', required=False, readonly=True)
     product_origin = fields.Char(string='Producto origen', required=True, readonly=True)
-    sale_origin = fields.Char(string='Venta', required=True, readonly=True)
-    sale_name = fields.Char(string='Compra', required=False, readonly=True)
-    product_code = fields.Char(string='Código de producto', required=True, readonly=True)
+    purchase_partner = fields.Char(string='Proveedor', required=False, readonly=True)
+    product_code = fields.Char(string='Código', required=True, readonly=True)
     product_color = fields.Char(string='Color', required=False, readonly=True)
     product_material = fields.Char(string='Material', required=False, readonly=True)
     material_code = fields.Char(string='CM', required=False, readonly=True)
     color_code = fields.Char(string='CC', required=False, readonly=True)
-    product_name = fields.Char(string='Producto', required=True, readonly=True)
+    product_name = fields.Char(string='Nombre', required=True, readonly=True)
     product_quantity = fields.Float(string='Cantidad', required=False, readonly=True, store=True)
     product_uom_name = fields.Char(string='UoM', required=True, readonly=True)
     model_id = fields.Integer(string='Id del Modelo', required=True, readonly=True)
     type = fields.Char(string='Tipo', required=True, readonly=True)
-    type_model_id = fields.Char(string='Código', required=True, readonly=True)
+    type_model_id = fields.Char(string='Ref. Interna', required=True, readonly=True)
     checked = fields.Boolean(string='Procesada', required=False)
-
+    is_finished_line = fields.Boolean(string='Fabricado', compute='compute_is_finished_line', store=True)
     lst = fields.Selection(string='01-LST',
                            selection=lambda self: dynamic_selection(), required=False, readonly=False)
     lsc = fields.Selection(string='02-LSC',
@@ -48,13 +54,6 @@ class Supplierlist(models.Model):
                            selection=lambda self: dynamic_selection(), required=False, readonly=False)
     pin = fields.Selection(string='07-PIN',
                            selection=lambda self: dynamic_selection(), required=False, readonly=False)
-
-    # sec = fields.Selection(string='SEC',
-    #                        selection=lambda self: dynamic_selection(), required=False, readonly=False)
-    # emb = fields.Selection(string='EMB',
-    #                        selection=lambda self: dynamic_selection(), required=False, readonly=False)
-
-    is_finished_line = fields.Boolean(string='Fabricado', compute='compute_is_finished_line', store=True)
 
     @api.depends('lst', 'lsc', 'plg', 'cmz', 'man', 'sol', 'pin')
     def compute_is_finished_line(self):
@@ -77,22 +76,13 @@ class Supplierlist(models.Model):
         po_origin_list = list(set(filter(lambda x: x[0:2] == 'OP', po_origin_list)))
 
         for orden_principal in po_origin_list:
-            sm_ids = self.env['stock.move'].search([('name', '=', 'OP/00600')])  # 'OP/00516' orden_principal
-            po_id = self.env['purchase.order'].search([('origin', 'ilike', 'OP/00600'),
-                                                       ('sale_order_id', '!=', False)])
-            po_id_metal = self.env['purchase.order'].search([('origin', 'ilike', 'OP/00600'),
-                                                             ('sale_order_id', '=', False)])
-            if len(po_id):
-                purchasename = po_id[0].name
-            else:
-                purchasename = 'NA'
-
-            if len(po_id_metal):
-                purchasenamemetal = po_id_metal[0].name
-            else:
-                purchasenamemetal = 'NA'
+            orden_principal = 'OP/00600'  # Sólo testing
+            sm_ids = self.env['stock.move'].search([('name', '=', orden_principal),
+                                                    ('created_purchase_line_id', '!=', False)])
 
             for sm in sm_ids:
+                purchaseorder = sm.created_purchase_line_id.order_id.id
+                purchasepartner = sm.created_purchase_line_id.order_id.partner_id.name
                 if sm.product_id.default_code[0:3] in ('A30', 'A31') and \
                         sm['id'] not in self._get_supplier_list_ids_for_sm():
 
@@ -113,12 +103,14 @@ class Supplierlist(models.Model):
                     self.create({'checked': False,
                                  'partner_name': mo_id.sale_id.partner_id.name,
                                  'commitment_date': mo_id.sale_id.commitment_date,
-                                 'sale_origin': mo_id.origin,
+                                 'sale_order': mo_id.sale_id.id,
                                  'product_origin': '[' + mo_id.product_id.default_code + ']' + ' ' +
                                                    mo_id.product_id.name,
-                                 'manufacturing_origin': sm.origin,
-                                 'sale_name': purchasename,
-                                 'product_code': sm.product_id.default_code,
+                                 'mrp_production': self.env['mrp.production'].search([('name', '=', sm.origin)]).id,
+                                 'purchase_order': purchaseorder,
+                                 'purchase_partner': purchasepartner,
+                                 'product_template': sm.product_tmpl_id.id,
+                                 'product_code': sm.product_tmpl_id.default_code,
                                  'product_name': sm.product_id.name,
                                  'product_quantity': sm.product_qty,
                                  'product_uom_name': sm.product_uom.name,
@@ -137,38 +129,42 @@ class Supplierlist(models.Model):
                                  'sol': '1' if 'SOL' in route_list else '',
                                  'pin': '1' if 'PIN' in route_list else '',
                                  })
-                    self.your_function2(purchasenamemetal, materialname, materialcode)
+                    self.your_function2(materialname, materialcode, orden_principal)
         res = 'Good Job'
         return res
 
     @api.model
-    def your_function2(self, purchasenamemetal, materialname, materialcode):
+    def your_function2(self, materialname, materialcode, orden_principal):
         route_list = []
         route_list2 = []
-        supplier_list_ids = self.env['supplier.list'].search([])
+        supplier_list_ids = self.env['supplier.list'].search([('checked', '=', False)])
         for record in supplier_list_ids:
             if not record['checked']:
                 record.write({'checked': True})
-                code = record.product_code
+                code = record.product_template.default_code
                 bom_ids_max = self._get_bom(code)
-                # for bom in bom_ids_max:
-                #     print('N1', bom.product_tmpl_id.default_code, bom.vault_route)
                 x = 0
                 for bom_line in bom_ids_max.bom_line_ids:
                     code = bom_line.product_id.default_code
                     bom_ids_max2 = self._get_bom(code)
                     for bom in bom_ids_max2:
                         route_list += bom['vault_route'].split("-", -1)
+                        purchaseorder = self.env['purchase.order.line'].search([
+                            ('product_template_id.default_code', '=', bom.product_tmpl_id.default_code),
+                            ('order_id.origin', 'ilike', orden_principal)
+                        ]).order_id,
                         x = x + 1
                         self.create({'checked': True,
                                      'partner_name': self.env['sale.order'].
-                                    search([('name', '=', record.sale_origin)]).partner_id.name,
+                                    search([('name', '=', record.sale_order.name)]).partner_id.name,
                                      'commitment_date': self.env['sale.order'].
-                                    search([('name', '=', record.sale_origin)]).commitment_date,
-                                     'sale_origin': record.sale_origin,
+                                    search([('name', '=', record.sale_order.name)]).commitment_date,
+                                     'sale_order': record.sale_order.id,
                                      'product_origin': record.product_origin,
-                                     'manufacturing_origin': record.manufacturing_origin,
-                                     'sale_name': purchasenamemetal,
+                                     'mrp_production': record.mrp_production.id,
+                                     'purchase_order': purchaseorder[0].id,
+                                     'purchase_partner': purchaseorder[0].partner_id.name,
+                                     'product_template': bom.product_tmpl_id.id,
                                      'product_code': '-- ' + bom.product_tmpl_id.default_code,
                                      'product_name': bom.product_tmpl_id.name,
                                      'product_quantity': record.product_quantity * bom.product_qty,
@@ -194,18 +190,18 @@ class Supplierlist(models.Model):
                         bom_ids_max3 = self._get_bom(code)
                         for bom in bom_ids_max3:
                             if bom['vault_route']:
-                                # print('    N3', bom.product_tmpl_id.default_code, bom.vault_route)
                                 route_list2 += bom['vault_route'].split("-", -1)
                                 y = y + 1
                                 self.create({'checked': True,
                                              'partner_name': self.env['sale.order'].
-                                            search([('name', '=', record.sale_origin)]).partner_id.name,
+                                            search([('name', '=', record.sale_order.name)]).partner_id.name,
                                              'commitment_date': self.env['sale.order'].
-                                            search([('name', '=', record.sale_origin)]).commitment_date,
-                                             'sale_origin': record.sale_origin,
+                                            search([('name', '=', record.sale_order.name)]).commitment_date,
+                                             'sale_order': record.sale_order.id,
                                              'product_origin': record.product_origin,
-                                             'manufacturing_origin': record.manufacturing_origin,
-                                             'sale_name': '',
+                                             'mrp_production': record.mrp_production.id,
+                                             'purchase_order': '',
+                                             'product_template': bom.product_tmpl_id.id,
                                              'product_code': '------ ' + bom.product_tmpl_id.default_code,
                                              'product_name': bom.product_tmpl_id.name,
                                              'product_quantity': record.product_quantity * bom_line2.bom_id.product_qty * bom.product_qty,
@@ -229,18 +225,18 @@ class Supplierlist(models.Model):
                             for bom_line3 in bom_ids_max3.bom_line_ids:
                                 code = bom_line3.product_id.default_code
                                 bom_ids_max4 = self._get_bom(code)
-                                # print('      N4', code)
                                 z = z + 1
                                 self.create({'checked': True,
                                              'partner_name': self.env['sale.order'].
-                                            search([('name', '=', record.sale_origin)]).partner_id.name,
+                                            search([('name', '=', record.sale_order.name)]).partner_id.name,
                                              'commitment_date': self.env['sale.order'].
-                                            search([('name', '=', record.sale_origin)]).commitment_date,
-                                             'sale_origin': record.sale_origin,
+                                            search([('name', '=', record.sale_order.name)]).commitment_date,
+                                             'sale_order': record.sale_order.id,
                                              'product_origin': record.product_origin,
-                                             'manufacturing_origin': record.manufacturing_origin,
-                                             'sale_name': '',
-                                             'product_code': '---------- ' + bom_line3.product_id.default_code,
+                                             'mrp_production': record.mrp_production.id,
+                                             'purchase_order': '',
+                                             'product_template': bom_line3.product_tmpl_id.id,
+                                             'product_code': '---------- ' + bom.product_tmpl_id.default_code,
                                              'product_name': bom_line3.product_id.name,
                                              'product_quantity': record.product_quantity * bom_line3.bom_id.product_qty * bom_line2.bom_id.product_qty * bom_line3.product_qty,
                                              'product_uom_name': bom_line3.product_uom_id.name,
