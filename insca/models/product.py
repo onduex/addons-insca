844b8da81bcd043ed184cbadce362712c807417d
nachoserra-domatix <nacho.serra@domatix.com>
2022-09-21 13:26:01 +0200

    [FIX] Si no hay name en vals falla

--- insca/models/product.py
+++ insca/models/product.py
@@ -208,15 +208,16 @@ class ProductTemplate(models.Model):
                                     product_ids = self.env['product.product'].search([('default_code', '=',
                                                                                        vals['vault_material_code'])])
                                 for product in product_ids:
-                                    if vals['name'][0:3] == 'CPF' or vals['name'][0:3] == 'CPC':
-                                        qty = vals['vault_sup_madera']
-                                    if vals['name'][0:2] == 'TR' or \
-                                            vals['name'][0:2] == 'TC' or \
-                                            vals['name'][0:2] == 'TO' or \
-                                            vals['name'][0:2] == 'MZ':
-                                        qty = vals['vault_length_tub']
-                                    lines.append((0, 0, {'product_id': product.id, 'product_qty': qty}))
-                                    qty = None
+                                    if vals.get('name'):
+                                        if vals['name'][0:3] == 'CPF' or vals['name'][0:3] == 'CPC':
+                                            qty = vals['vault_sup_madera']
+                                        if vals['name'][0:2] == 'TR' or \
+                                                vals['name'][0:2] == 'TC' or \
+                                                vals['name'][0:2] == 'TO' or \
+                                                vals['name'][0:2] == 'MZ':
+                                            qty = vals['vault_length_tub']
+                                        lines.append((0, 0, {'product_id': product.id, 'product_qty': qty}))
+                                        qty = None
                                 mrp_bom_object.sudo().create({'product_tmpl_id': self.id,
                                                               'code': self.vault_revision,
                                                               'product_qty': 1,
