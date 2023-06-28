c586d8a9f42a9e05e9de57b939f34f33461b0036
Tomás Pascual <tomas.pascual@onduex.com>
2023-06-28 15:16:38 +0200

    [IMP] update

--- insca/models/__init__.py
+++ insca/models/__init__.py
@@ -5,4 +5,4 @@ from . import product
 from . import res_code
 from . import product_supplier_info
 from . import mrp_workorder
-from . import bom_product_is_old_revision
+# from . import bom_product_is_old_revision
