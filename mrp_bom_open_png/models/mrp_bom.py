1407a6865f3c87425867bc7c40b479810f9f2938
Tomás Pascual <tomas.pascual@onduex.com>
2023-09-13 16:03:30 +0200

    [NEW MODULE] add local path to png from Bom and BomLines in BoM report

--- mrp_bom_open_png/models/mrp_bom.py
+++ mrp_bom_open_png/models/mrp_bom.py
@@ -45,29 +45,33 @@ class MrpBomLine(models.Model):
     def create(self, vals_list):
         res = super().create(vals_list)
         for bomLine in res:
-            suffix = ''
-            if len(bomLine.bom_id.code) == 1:
-                suffix = "_R0" + bomLine.bom_id.code
-            elif len(bomLine.bom_id.code) == 2:
-                suffix = "_R" + bomLine.bom_id.code
-            bomLine.png_link = ("R:/DTECNIC/PLANOS/0_PNG/" + bomLine.product_id.default_code[0:3] + "/" +
-                                bomLine.product_id.default_code[0:7] + "/" +
-                                bomLine.product_id.default_code + suffix + ".png")
-            return res
+            if bomLine.product_id.is_vault_product:
+                suffix = ''
+                if len(bomLine.product_id.vault_revision) == 1:
+                    suffix = "_R0" + bomLine.product_id.vault_revision
+                elif len(bomLine.product_id.vault_revision) == 2:
+                    suffix = "_R" + bomLine.product_id.vault_revision
+                bomLine.png_link = ("R:/DTECNIC/PLANOS/0_PNG/" + bomLine.product_id.default_code[0:3] + "/" +
+                                    bomLine.product_id.default_code[0:7] + "/" +
+                                    bomLine.product_id.default_code + suffix + ".png")
+        return res
+
 
     def write(self, values):
         super(MrpBomLine, self).write(values)
         if values.get('product_id'):
             for bomLine in self:
-                suffix = ''
-                if len(bomLine.bom_id.code) == 1:
-                    suffix = "_R0" + bomLine.bom_id.code
-                elif len(bomLine.bom_id.code) == 2:
-                    suffix = "_R" + bomLine.bom_id.code
-                values.update({'png_link': ("R:/DTECNIC/PLANOS/0_PNG/" + bomLine.product_id.default_code[0:3] + "/" +
-                                            bomLine.product_id.default_code[0:7] + "/" +
-                                            bomLine.product_id.default_code + suffix + ".png")
-                               })
-                return super(MrpBomLine, bomLine).write(values)
+                if bomLine.product_id.is_vault_product:
+                    suffix = ''
+                    if len(bomLine.product_id.vault_revision) == 1:
+                        suffix = "_R0" + bomLine.product_id.vault_revision
+                    elif len(bomLine.product_id.vault_revision) == 2:
+                        suffix = "_R" + bomLine.product_id.vault_revision
+                    values.update(
+                        {'png_link': ("R:/DTECNIC/PLANOS/0_PNG/" + bomLine.product_id.default_code[0:3] + "/" +
+                                      bomLine.product_id.default_code[0:7] + "/" +
+                                      bomLine.product_id.default_code + suffix + ".png")
+                         })
+                    return super(MrpBomLine, self).write(values)
 
     png_link = fields.Char(string='PNG', required=False, readonly=False, store=True)
