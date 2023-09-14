84a27806a8d56b6952f93132d20ba6883c571abe
Tomás Pascual <tomas.pascual@onduex.com>
2023-09-14 10:25:52 +0200

    [NEW MODULE] add local path to png from Bom and BomLines in BoM report

--- mrp_bom_open_png/models/mrp_bom.py
+++ mrp_bom_open_png/models/mrp_bom.py
@@ -4,39 +4,32 @@
 from odoo import fields, models, api, _
 
 
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.depends('vault_internal_id')
+    def _set_png_link(self):
+        for product in self:
+            if (product.default_code and product.default_code[0:3] == 'A00') or product.is_vault_product:
+                suffix = ''
+                if len(str(product.vault_revision)) == 1:
+                    suffix = "_R0" + product.vault_revision
+                elif len(str(product.vault_revision)) == 2:
+                    suffix = "_R" + product.vault_revision
+                product.png_link = ("R:/DTECNIC/PLANOS/0_PNG/" + product.default_code[0:3] + "/" +
+                                    product.default_code[0:7] + "/" +
+                                    product.default_code + suffix + ".png")
+
+    png_link = fields.Char(string='PNG', store=True, compute='_set_png_link')
+
+
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
-    @api.depends('product_tmpl_id', 'product_id', 'product_tmpl_id.vault_internal_id')
-    def _set_png_link(self):
-        for bom in self:
-            if bom.product_tmpl_id.default_code[0:3] == 'A00' or bom.product_tmpl_id.is_vault_product:
-                suffix = ''
-                if len(str(bom.product_tmpl_id.vault_revision)) == 1:
-                    suffix = "_R0" + bom.product_tmpl_id.vault_revision
-                elif len(str(bom.product_tmpl_id.vault_revision)) == 2:
-                    suffix = "_R" + bom.product_tmpl_id.vault_revision
-                bom.png_link = ("R:/DTECNIC/PLANOS/0_PNG/" + bom.product_tmpl_id.default_code[0:3] + "/" +
-                                bom.product_tmpl_id.default_code[0:7] + "/" +
-                                bom.product_tmpl_id.default_code + suffix + ".png")
-
-    png_link = fields.Char(string='PNG', required=False, readonly=False, store=True, compute='_set_png_link')
+    png_link = fields.Char(string='PNG', store=True, related='product_tmpl_id.png_link')
 
 
 class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
-    @api.depends('product_id', 'product_tmpl_id.vault_revision', 'product_tmpl_id.vault_internal_id')
-    def _set_png_link(self):
-        for bomLine in self:
-            if bomLine.product_tmpl_id.default_code[0:3] == 'A00' or bomLine.product_tmpl_id.is_vault_product:
-                suffix = ''
-                if len(str(bomLine.product_tmpl_id.vault_revision)) == 1:
-                    suffix = "_R0" + bomLine.product_tmpl_id.vault_revision
-                elif len(str(bomLine.product_tmpl_id.vault_revision)) == 2:
-                    suffix = "_R" + bomLine.product_tmpl_id.vault_revision
-                bomLine.png_link = ("R:/DTECNIC/PLANOS/0_PNG/" + bomLine.product_tmpl_id.default_code[0:3] + "/" +
-                                    bomLine.product_tmpl_id.default_code[0:7] + "/" +
-                                    bomLine.product_tmpl_id.default_code + suffix + ".png")
-
-    png_link = fields.Char(string='PNG', required=False, readonly=False, store=True, compute='_set_png_link')
+    png_link = fields.Char(string='PNG', store=True, related='product_tmpl_id.png_link')
