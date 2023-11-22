6dc7c051a9d023c59d06be68458c2d209ce2751c
Tomás Pascual <tomas.pascual@onduex.com>
2023-11-22 10:27:38 +0100

    [IMP] print bom wizard

--- insca3/wizards/print_bom_wiz.py
+++ insca3/wizards/print_bom_wiz.py
@@ -73,6 +73,7 @@ class PrintBomWiz(models.TransientModel):
 
     def get_all_bom_lines_without_hrj(self):
         self.remove_bom_lines()
+        lines_without_hrj = []
         i = 0
         for o in self.bom_id:
             i += 1
@@ -80,11 +81,46 @@ class PrintBomWiz(models.TransientModel):
             for ch in o.bom_line_ids:
                 i = self.print_all_bom_children_with_bom(ch, i, j)
 
-        print(lines)
-        print('eureka')
+        for line in i:
+            if line[2]['default_code'][0:4] == 'A70.':
+                line[2]['to_print'] = False
+                lines_without_hrj.append(line)
+            else:
+                lines_without_hrj.append(line)
 
         context = {'default_bom_id': self.bom_id.id,
-                   'default_bom_line_ids': lines}
+                   'default_bom_line_ids': lines_without_hrj}
+
+        return {
+            'name': 'Imprimir Lista de Materiales',
+            'type': 'ir.actions.act_window',
+            'res_model': 'print.bom.wiz',
+            'context': context,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': 2787,
+            'target': 'new'}
+
+    def get_all_bom_lines_only_ptg(self):
+        self.remove_bom_lines()
+        lines_only_ptg = []
+        i = 0
+        for o in self.bom_id:
+            i += 1
+            j = 0
+            for ch in o.bom_line_ids:
+                i = self.print_all_bom_children_with_bom(ch, i, j)
+
+        for line in i:
+            if line[2]['default_code'][0:4] == 'A10.' or line[2]['default_code'][0:4] == 'A11.':
+                line[2]['to_print'] = True
+                lines_only_ptg.append(line)
+            else:
+                line[2]['to_print'] = False
+                lines_only_ptg.append(line)
+
+        context = {'default_bom_id': self.bom_id.id,
+                   'default_bom_line_ids': lines_only_ptg}
 
         return {
             'name': 'Imprimir Lista de Materiales',
