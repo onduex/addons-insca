233d53a279df5eac3dd979796dad10d674bf4dbb
Tomás Pascual <tomas.pascual@onduex.com>
2024-02-12 17:14:33 +0100

    [IMP] insca5 report herrajes update

--- insca5/__manifest__.py
+++ insca5/__manifest__.py
@@ -2,29 +2,26 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "insca5",
+    'summary': """
+        Módulo impresión report herrajes por orden de trabajo montaje""",
+
+    'description': """
+        Módulo impresión report herrajes por orden de trabajo montaje""",
+
     'version': '13.0',
     'author': 'Onduex sl',
     'website': 'http://www.onduex.com',
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['mrp',
-                'mrp_workorder',
+    'depends': ['mrp'
                 ],
 
     "data": [
-        # 'views/mrp_bom_view.xml',
-        # 'views/mrp_production_menu.xml',
-        # 'views/mrp_bom_component_view.xml',
-        # 'views/product_template.xml',
-        # 'views/hr_timesheet.xml',
-
+        'reports/external_layout_workorder.xml',
+        'reports/external_layout_boxed_workorder.xml',
         'reports/declare.xml',
-        'reports/report_paperformat.xml',
-        # 'reports/report_hoja_material3.xml',
-        # 'reports/report_hoja_material2.xml',
-        # 'reports/report_hoja_material.xml',
-        # 'reports/report_hoja_compras.xml',
+        'reports/report_herrajes.xml',
     ],
 
     "installable": True,
